e6ee93a87d491aa17d7167687944271aae351e20
John-Trunix <john@trunix.ch>
2020-12-26 19:14:36 +0100

    Version 1.1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MMM-BluetoothDetection
-An easy way to detect if youre Phone is in range and turns Mirror on or off.
+An easy way to detect if youre Phone is in bluetooth range and turns Mirror on or off.
+
+More is comming soon...
--- pingbluetooth.py
+++ pingbluetooth.py
@@ -1,8 +1,12 @@
-import os
-import time
+#Made by John-Trunix, free to use!
+#Add this file to crontab for periodic execution
 
+import os
+
+#Add your bluetooth Adress
 bluetoth_status = os.system('sudo l2ping -c1 -s32 -t1 XX:XX:XX:XX:XX:XX')
 
+#If it can ping successfully, you will get code 0 for successful
 if bluetoth_status == 0:
     os.system("vcgencmd display_power 1")
 
