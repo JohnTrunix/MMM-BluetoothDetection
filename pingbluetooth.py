94301275004a2d4d10b2e09864dccbba6d83b382
JohnTrunix <61423865+JohnTrunix@users.noreply.github.com>
2021-02-08 11:56:32 +0100

    Update pingbluetooth.py

--- pingbluetooth.py
+++ pingbluetooth.py
@@ -6,7 +6,7 @@ import os
 #Add your bluetooth Adress
 bluetoth_status = os.system('sudo l2ping -c1 -s32 -t1 XX:XX:XX:XX:XX:XX')
 
-#If it can ping successfully, you will get code 0 for successful
+#If it can ping successfully, you will get code 0
 if bluetoth_status == 0:
     os.system("vcgencmd display_power 1")
 
